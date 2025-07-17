dd91e9e117690de9947214b35e85f946e5565427
Lý Thái Bảo <baoly069@gmail.com>
2025-07-17 19:40:38 +0700

    js

--- main.js
+++ main.js
@@ -5,17 +5,31 @@
 //   });
 
 const taskApi = "https://todo-list-api-whgl.onrender.com/group1";
+var time = new Date();
+console.log(time.getMinutes());
+
+// function autoScrollWeb() {
+//   setInterval(() => {
+//     // đợi DOM cập nhật xong hết mới cuộn
+//     var boxList = document.querySelector(".list__task");
+//     boxList.scrollTop = boxList.scrollHeight; // tự động scoll xuống
+//   }, 10000);
+// }
+
+// autoScrollWeb();
 function start() {
   getTasks(reRenderList);
   handleValueUserByButton();
 }
 start();
 function getTasks(callback) {
-  setInterval(() => {
-    fetch(taskApi)
-      .then((response) => response.json())
-      .then(callback);
-  }, 2000);
+  setTimeout(() => {
+    setInterval(() => {
+      fetch(taskApi)
+        .then((response) => response.json())
+        .then(callback);
+    }, 2000);
+  }, 1000);
 }
 function reRenderList(tasks) {
   var containList = document.querySelector(".list__task");
@@ -24,8 +38,8 @@ function reRenderList(tasks) {
       (infor) => `
           <div class ="list__item">
             <div class = "main__task">
-              <div><i class="fa-regular fa-bell" style = "padding-right: 10px;color: green"></i><strong>${infor.name}</strong></div>
               <p><i class="fa-solid fa-comment"  style = "padding-right: 10px;color: blue""></i>${infor.content}</p>
+              <small class= "time">${infor.time}</small>
             </div>
             <div class = "remove__task" onclick = "handleDeleteTask(${infor.id})">
             <i class="fa-solid fa-trash"></i>
@@ -57,17 +71,26 @@ function handleValueUserByButton() {
   var btnAdd = document.querySelector(".add__task");
   btnAdd.addEventListener("click", (e) => {
     e.preventDefault();
-    var ten = document.querySelector('input[name="name"]');
+    // var ten = document.querySelector('input[name="name"]');
     var noiDung = document.querySelector('input[name="content"]');
     var taskForm = {
-      name: ten.value,
+      time: `${new Date().getDate()}/${
+        new Date().getMonth() + 1
+      }/${new Date().getFullYear()}<i class="fa-solid fa-clock" style = "padding-left:5px"></i> ${new Date().getHours()}:${new Date().getMinutes()}`,
       content: noiDung.value,
     };
-    if (ten !== "" && noiDung !== "") {
+
+    if (noiDung.value !== "") {
       createTask(taskForm);
+      setTimeout(() => {
+        // đợi DOM cập nhật xong hết mới cuộn
+        var boxList = document.querySelector(".list__task");
+        boxList.scrollTop = boxList.scrollHeight; // tự động scoll xuống
+      }, 2000);
     }
-    ten.value = " ";
-    noiDung.value = " ";
+    // ten.value = " ";
+    noiDung.value = "";
+    noiDung.focus();
   });
 }
 
