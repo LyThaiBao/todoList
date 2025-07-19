dfdfe8de3d00c1f727586b6ad5b61f4892755320
Lý Thái Bảo <baoly069@gmail.com>
2025-07-19 13:34:14 +0700

    fix

--- explore/explore.js
+++ explore/explore.js
@@ -2,8 +2,38 @@ const articleApi = "https://todo-list-api-whgl.onrender.com/article";
 
 fetch(articleApi)
   .then((response) => response.json())
-  .then((articles) => {
-    console.log(articles);
+  .then((article) => {
+    console.log(article.status);
     var articleThumb = document.querySelector(".article__thumb");
-    articleThumb.innerHTML = `<img src="${articles.image}"/>`;
+    articleThumb.innerHTML = `<img src="${article.image}"/>`;
   });
+function start() {
+  getArticle(reRenderArticle);
+}
+function getArticle(callback) {
+  fetch(articleApi)
+    .then((response) => response.json())
+    .then(callback);
+}
+
+function reRenderArticle(articles) {
+  articles.map(
+    (article) => `
+      <div class="article__item">
+          <h4 class="article__status">${article.status}</h4>
+          <div class="article__thumb">
+            <img src="${article.image}" alt="" /> 
+          </div>
+          <div class="article__cta">
+            <span class="article__like">
+              <i class="fa-regular fa-heart"></i>
+              0
+            </span>
+            <span class="article__comment">
+              <i class="fa-solid fa-comments"></i>
+            </span>
+          </div>
+      </div>
+`
+  );
+}
